ff6a866a7490ab9112cdb20d0d30a79389e61f6b
Rick van Blitterswijk <Rick.van.Blitterswijk@gorillait.nl>
2021-01-06 19:39:53 +0100

    add player, jumpheight, fireball, platform and background

---
+++ Assets/scripts/B.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+public class B : MonoBehaviour
+{
+    [Header("Attack")]
+    public GameObject fireBall;
+
+    [Header("Movement")]
+    public float runSpeed;
+    public float jumpForce;
+
+    [Header("Collision")]
+    public float groundedCheckRadius;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private CapsuleCollider2D capsuleCollider2D;
+
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(new Vector2(transform.position.x, capsuleCollider2D.bounds.min.y), groundedCheckRadius, groundLayer);
+
+        JumpCheck();
+        FireBallCheck();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveHorizontally();
+    }
+
+    private void FireBallCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CastFireBall();
+        }
+    }
+
+    private void CastFireBall()
+    {
+        Instantiate(fireBall, transform.position, transform.rotation);
+    }
+
+    private void JumpCheck()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+            if (animator.GetBool("isFalling"))
+            {
+                animator.SetBool("isFalling", false);
+            }
+        }
+        else if (!isGrounded)
+        {
+            if (rigidbody2D.velocity.y < 0f)
+            {
+                animator.SetBool("isFalling", true);
+            } else if (rigidbody2D.velocity.y > 1f && Input.GetKeyUp(KeyCode.Space))
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        animator.SetTrigger("jump");
+    }
+
+    private void MoveHorizontally()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            MoveRight();
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            MoveLeft();
+        } else
+        {
+            StopHorizontalMovement();
+        }
+    }
+
+    private void MoveRight()
+    {
+        rigidbody2D.velocity = new Vector2(runSpeed * Input.GetAxis("Horizontal"), rigidbody2D.velocity.y);
+        transform.localScale = new Vector2(1, transform.localScale.y);
+        animator.SetBool("isRunning", true);
+    }
+
+    private void MoveLeft()
+    {
+        rigidbody2D.velocity = new Vector2(runSpeed * Input.GetAxis("Horizontal"), rigidbody2D.velocity.y);
+        transform.localScale = new Vector2(-1, transform.localScale.y);
+        animator.SetBool("isRunning", true);
+    }
+
+    private void StopHorizontalMovement()
+    {
+        rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
+        animator.SetBool("isRunning", false);
+    }
+}
---
+++ Assets/scripts/FireBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float travelSpeed;
+    public float maxLifetime;
+
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        FlyTowardsCursor();
+        Destroy(gameObject, maxLifetime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FlyTowardsCursor()
+    {
+        var direction = (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        direction.Normalize();
+
+        transform.right = direction;
+        rigidbody2D.velocity = direction * travelSpeed;
+    }
+}
